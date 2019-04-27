6266b3f2374492922b69b2909b95b8626b6b7733
Toby Qin <toby.qin@live.com>
2019-04-27 15:20:34 +0800

    easier to debug locally.

--- run.py
+++ run.py
@@ -2,8 +2,16 @@
 To run and debug locally.
 """
 import os
+from os import path
+from shutil import copyfile
 
 from wiki.web import create_app
 
-app = create_app(os.path.dirname(__file__) + '/content')
+content_dir = path.join(path.dirname(__file__), 'content')
+
+if not path.exists(content_dir):
+    os.mkdir(content_dir)
+    copyfile('config.example.py', './content/config.py')
+
+app = create_app(content_dir)
 app.run(debug=True, host='127.0.0.1', port=5000)
